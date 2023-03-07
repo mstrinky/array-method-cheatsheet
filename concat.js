3458ae35ed19388a0ec7d97abd08e1d1ac49603d
Mst Rinky Akter <mstrinkya@gmail.com>
2023-03-07 10:13:47 +0600

    concat method

---
+++ concat.js
@@ -0,0 +1,7 @@
+// The concat() method is used to join two or more arrays
+
+var elderSisters = [ 'pinky','rinky'];
+var yongerSisters = ['chumky','asa'];
+var parents =['ataur','selina']
+var together = elderSisters.concat(yongerSisters).concat(parents);
+console.log(together);
